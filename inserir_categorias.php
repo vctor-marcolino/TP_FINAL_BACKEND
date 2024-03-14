bd76ca87c9dd9f244a86bcc0429237c028202324
wesleydev1BH <vctor.marcolino@gmail.com>
2024-03-13 21:58:15 -0300

    formulario inserir produto

--- inserir_categorias.php
+++ inserir_categorias.php
@@ -1,9 +1,10 @@
-<?php
+<?php 
     echo $_GET["nome"];
 
     $pdo = new PDO("sqlite:estoque.db");
 
     $queryInsereCategoria = "INSERT INTO categorias (nome)
-        VALUES (\"".$_GET["nome"]."\")";
-            $pdo ->query($queryInsereCategoria);
+	VALUES (\"".$_GET["nome"]."\")";
+
+    $pdo->query($queryInsereCategoria);
 ?>
